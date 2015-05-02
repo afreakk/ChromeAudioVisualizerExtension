139f6867d255bb570d589e3386703bb22846cf93
afreakk <laderud@hotmail.com>
2015-05-02 16:22:44 +0200

    bugfix+refactor

--- contentScripts/sceneManager.js
+++ contentScripts/sceneManager.js
@@ -1,39 +1,8 @@
-function togglePause()
-{
-	console.log("toggling pause");
-	g.pause = !g.pause;
-	if(g.pause)
-	{
-		g.canvas.style.visibility = "hidden";
-		g.canvas.style.pointerEvents = "none";
-		g.datStyle.visibility = "hidden";
-		g.stats.domElement.style.visibility = 'hidden';
-	}
-	else
-	{
-		g.canvas.style.visibility = "visible";
-		g.canvas.style.pointerEvents = "auto";
-		g.datStyle.visibility = "visible";
-		g.stats.domElement.style.visibility = 'visible';
-		g.sceneManager.update();
-	}
-}
-function canvasResize()
-{
-	g.canvas.width = window.innerWidth;
-	g.canvas.height = window.innerHeight;
-	var top = window.scrollY.toString()+"px";
-	var left = window.scrollX.toString()+"px";
-	g.canvas.style.top = top;
-	g.canvas.style.left = left;
-	g.stats.domElement.style.top = top;
-	g.stats.domElement.style.left = left;
-}
 var SceneManager = function(scenes, sceneSelector, gui)
 {
+    this.scenes = scenes;
     this.sceneSelector = sceneSelector;
     this.gui = gui;
-    this.scenes = scenes;
     this.currentScene = null;
     this.initNewScene();
 };
@@ -45,10 +14,13 @@ SceneManager.prototype.update = function()
 		g.stats.begin();
 
 		canvasResize();
+
 		if(g.byteFrequency)
 			this.currentScene.update();
+
 		if(this.sceneSelector.scene != this.currentScene.name)
 			this.initNewScene();
+
 		g.port.postMessage("r");
 
 		g.stats.end();
@@ -62,19 +34,4 @@ SceneManager.prototype.initNewScene = function()
     this.currentScene = this.scenes[this.sceneSelector.scene];
     this.gui.refresh(this.currentScene.settings);
     this.currentScene.init();
-};
-
-var GUI = function(datGUI)
-{
-    this.gui = datGUI;
-    this.guiElements = [];
-};
-
-GUI.prototype.refresh = function(elements)
-{
-    while(this.guiElements.length>0)
-        this.gui.remove(this.guiElements.pop());
-    for(var elem in elements)
-        if(elements.hasOwnProperty(elem))
-            this.guiElements.push(this.gui.add(elements, elem));
 };
--- contentScripts/tools.js
+++ contentScripts/tools.js
@@ -8,3 +8,50 @@ function rgbToHex(r, g, b)
 {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 } 
+
+function togglePause()
+{
+	console.log("toggling pause");
+	g.pause = !g.pause;
+	if(g.pause)
+	{
+		g.canvas.style.visibility = "hidden";
+		g.canvas.style.pointerEvents = "none";
+		g.datStyle.visibility = "hidden";
+		g.stats.domElement.style.visibility = 'hidden';
+	}
+	else
+	{
+		g.canvas.style.visibility = "visible";
+		g.canvas.style.pointerEvents = "auto";
+		g.datStyle.visibility = "visible";
+		g.stats.domElement.style.visibility = 'visible';
+		g.sceneManager.update();
+	}
+}
+
+function canvasResize()
+{
+	g.canvas.width = window.innerWidth;
+	g.canvas.height = window.innerHeight;
+	var top = window.scrollY.toString()+"px";
+	var left = window.scrollX.toString()+"px";
+	g.canvas.style.top = top;
+	g.canvas.style.left = left;
+	g.stats.domElement.style.top = top;
+	g.stats.domElement.style.left = left;
+}
+
+var GUI = function(datGUI)
+{
+    this.gui = datGUI;
+    this.guiElements = [];
+};
+GUI.prototype.refresh = function(elements)
+{
+    while(this.guiElements.length>0)
+        this.gui.remove(this.guiElements.pop());
+    for(var elem in elements)
+        if(elements.hasOwnProperty(elem))
+            this.guiElements.push(this.gui.add(elements, elem));
+};
